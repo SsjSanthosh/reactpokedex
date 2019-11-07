53de7379d6c1c91db6fd9bcee80deab309f81855
Dextrous <ssjdexucyche@gmail.com>
2019-11-07 16:22:28 +0530

    Updates

---
+++ precache-manifest.7a2749b0d5899003ff1991d80e8b1dd2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5ad15db82712333705e92823c138469",
+    "url": "/reactpokedex/index.html"
+  },
+  {
+    "revision": "5dcf7c5079383094134a",
+    "url": "/reactpokedex/static/css/main.8c781e06.chunk.css"
+  },
+  {
+    "revision": "3c030a0be5873cb94e61",
+    "url": "/reactpokedex/static/js/2.04ccd6f1.chunk.js"
+  },
+  {
+    "revision": "5dcf7c5079383094134a",
+    "url": "/reactpokedex/static/js/main.ab8743e4.chunk.js"
+  },
+  {
+    "revision": "fb723d863ee24476870b",
+    "url": "/reactpokedex/static/js/runtime-main.0ca8f274.js"
+  }
+]);
